c095518ce59f4b04f73c4344dfad5c721090a603
HAPPY YADAV <happy.hy0017@gmail.com>
2024-08-01 23:16:20 +0530

    typescript

---
+++ first.js
@@ -0,0 +1,3 @@
+var user = { name: "happy", age: 45, salary: 5000000 };
+console.log("hey happy yadav");
+console.log(user.name);
---
+++ first.ts
@@ -0,0 +1,3 @@
+const user = { name: "happy", age: 45, salary: 5000000}
+console.log("hey happy yadav");
+console.log(user.name);
